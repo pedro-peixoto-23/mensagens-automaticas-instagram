46843514e207caca3f24e2b93f9b19bfb89e347f
Pedro Peixoto Viana de Oliveira <pedropeixoto54398@gmail.com>
2021-06-10 20:12:37 -0300

    adicionando inputs

--- instagram_messages.py
+++ instagram_messages.py
@@ -2,21 +2,47 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+import getpass as get
 import time
+import titles
+import os
 
 
 class InstagramMessages:
     def __init__(self):
+        os.system('cls')
+        titles.tit('Automatic messages for Instagram')
+        
         # Recebendo os dados essenciais para entrar no instagram
         self.username = input('\nUsername: ')
-        self.password = input('Password: ')
+        self.password = get.getpass('Password: ')
         
         # Mensagem que será enviada para os contatos
-        self.message = 'Hello! How are you?'
+        self.message = input('\nMessage: ')
+        
+        # Chamando o método responsável por receber e armazenar os contatos
+        self.contacts()
         
         # Abrir o google chrome e entrar no instagram
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.get('https://instagram.com')
+    
+    def contacts(self):
+        while True:
+            try:
+                contacts_number = int(input('\nHow many contacts will the '
+                                            'message be sent to? '))
+                break
+            except:
+                print('\033[1;31m' + 'ERROR: type a number!' + '\033[m')
+        print()
+        
+        # Lista que vai armazenar os contatos
+        self.contacts = list()
+        # Recebendo os contatos e armazenando na lista chamada "self.contacts"
+        for p in range(contacts_number):
+            p = input('Insert the {}º contact (@): '.format(p + 1))
+            self.contacts.append(p)
     
     def login(self):
         time.sleep(3)
@@ -42,11 +68,10 @@ class InstagramMessages:
         login_button_block = self.driver.find_element_by_xpath(xpath)
         # Clicar no botão
         login_button_block.click()
-    
-    # Quando chamar esse método, usar o @ da pessoa para quem quer enviar
-    def seek_contact(self, contact_name):
+       
+    def seek_contact(self):
         time.sleep(3)
-        for contact in contact_name:
+        for contact in self.contacts:
             self.driver.get(f'https://instagram.com/{contact}/')
             self.send_messages()
             
@@ -58,19 +83,18 @@ class InstagramMessages:
         # Clicar no botão de enviar mensagens
         send_message_button.click()
         # Tempo para apertar em não querer notificações
-        time.sleep(20)
+        time.sleep(10)
         
         # Procurar a caixa de mensagens
         message_block = self.driver.find_element_by_tag_name('textarea')
         # Clicar na caixa de texto
         message_block.click()
         # Inserir a mensagem na caixa
-        message_block.send_keys(self.mensagem)
+        message_block.send_keys(self.message)
         # Clicar ENTER para enviar
         message_block.send_keys(Keys.ENTER)
 
 
 instagram_messages = InstagramMessages()
 instagram_messages.login()
-# Observação: podem ser colocados mais de uma pessoa e até mesmo apenas uma
-instagram_messages.seek_contact(['contact_1', 'contact_2'])
+instagram_messages.seek_contact()
